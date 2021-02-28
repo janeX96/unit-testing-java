dd6e987803775df84a5a87f0b3db0b8ffde2dcb5
Janek <s16503@pjwstk.edu.pl>
2021-02-28 16:09:03 +0100

    test structure - given,when,then

--- src/test/java/testing/AccountTest.java
+++ src/test/java/testing/AccountTest.java
@@ -9,15 +9,22 @@ class AccountTest {
 
     @Test
     void myTest(){
+        //given
         Account account = new Account();
+
+        //then
         assertFalse(account.isActive(), "Check if new account is not active");
     }
 
     @Test
     void myTest2(){
+        //given
         Account account = new Account();
-        assertFalse(account.isActive());
+
+        //when
         account.activate();
+
+        //then
         assertTrue(account.isActive());
     }
 }
