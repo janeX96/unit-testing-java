16795fe8d4cb68969d91394d0d48d0c78326daa0
Janek <s16503@pjwstk.edu.pl>
2021-03-04 15:52:39 +0100

    testy wyjątków

--- src/test/java/testing/MealTest.java
+++ src/test/java/testing/MealTest.java
@@ -53,4 +53,14 @@ class MealTest {
         assertEquals(meal1,meal2,"Checking if two meals are equal");
         assertThat(meal1).isEqualTo(meal2);
     }
+
+    @Test
+    void exceptionShouldBeThrownIfDiscountIsHigherThanThePrice(){
+        //given
+        Meal meal = new Meal(8,"soup");
+
+        //when
+        //then
+        assertThrows(IllegalArgumentException.class,()->meal.getDiscountedPrice(5));
+    }
 }
